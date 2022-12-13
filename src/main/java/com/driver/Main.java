7287d11705ce002266671662d8fc25de98ee5b50
SaiVyshnavi08 <saivyshnavigudipalli@gmail.com>
2022-12-13 11:42:46 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+         RWOnly obj = new RWOnly();
+
+        //'name' has private access in 'com.driver.RWOnly'
+         // obj.name = "Vyshnavi";
+
+        obj.setName("Vyshnavi");
+        System.out.println(obj.getName());
+    }
 }
+
